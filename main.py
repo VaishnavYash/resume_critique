b2324eda44153939cd85c3faeb92af78f2806cdd
VaishnavYash <y01vaishnava@gmail.com>
2026-02-08 16:21:50 +0530

    limiter Added

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import os
 # from dotenv import load_dotenv
 from openai import OpenAI
-from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Request
+
 
 from config import constants
 from utils import text_extract_from_pdf as textFormat, utils
 from services import openai_services as services
 
 from core import new_resume_agent as resumeAgent, job_description_agent as jdAgent
+
+
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi import _rate_limit_exceeded_handler, Limiter
+
 # uv run uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+def user_key(request):
+    return request.headers.get("x-api-key") or get_remote_address(request)
+
+limiter = Limiter(key_func=user_key)
 
 # Load environment variables from .env file
-# load_dotenv() 
+# load_dotenv()
 
 # Initialize FastAPI app
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 # Initialize OpenAI client
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
@@ -32,11 +47,23 @@ def get_openai_client():
 client = get_openai_client() 
 
 @app.post("/analyze_resume_pdf")
+@limiter.limit("2/minute")
 async def analyze_resume_pdf(
+    request: Request, 
     resume: UploadFile = File(...),
     job_description: str = Form(constants.constJD)
 ):
     try:
+        if resume.content_type != "application/pdf":
+            raise HTTPException(
+                status_code=400,
+                detail={
+                    "status": "error",
+                    "code": "EMPTY_FILE",
+                    "message": "Only PDF files allowed"
+                }
+            )
+
         file_bytes = await resume.read()
 
         if not file_bytes:
@@ -84,11 +111,23 @@ async def analyze_resume_pdf(
         )
 
 @app.post("/get_pdf_data")
+@limiter.limit("2/minute")
 async def get_pdf_content(
+    request: Request,
     job_description: str = Form(constants.constJD),
     resume: UploadFile = File(...)
 ):
     try:
+        if resume.content_type != "application/pdf":
+            raise HTTPException(
+                status_code=400,
+                detail={
+                    "status": "error",
+                    "code": "EMPTY_FILE",
+                    "message": "Only PDF files allowed"
+                }
+            )
+        
         file_bytes = await resume.read()
 
         if not file_bytes:
